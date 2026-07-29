d32570358739fad3ef2640ccdf380ea1329f2b23
camelia-d-e <cades413@ulaval.ca>
2026-07-29 17:47:01 +0000

    fix: demarrer opcua-connector dans script spinup cnc

--- scripts/spinup_cnc.py
+++ scripts/spinup_cnc.py
@@ -4,19 +4,25 @@ import time
 def main():
     processes = []
     try:
-        print(" [1/3] Starting virtual cnc device+adapter...")
+        print(" [1/4] Starting virtual cnc device+adapter...")
         adapter_proc = subprocess.Popen(["python", "scripts/run_virtual_adapters.py", "cnc"])
         processes.append(adapter_proc)
         
         time.sleep(10)
 
-        print("\n [2/3] Bringing up OPC-UA device (lab-usine/plt-0313/cnc.yml)...")
-        device_proc = subprocess.Popen(["ofa", "device", "up", "lab-usine/plt-3013/cnc.yml"])
+        print(" [2/4] Starting OPCUA connector...")
+        connector_proc = subprocess.Popen(["ofa", "apps", "up", "/usr/local/share/openfactory-connectors/app_opcua_connector.yml"])
+        processes.append(connector_proc)
+        
+        time.sleep(10)
+
+        print("\n [2/4] Bringing up OPC-UA device (lab-usine/plt-0313/cnc.yml)...")
+        device_proc = subprocess.Popen(["ofa", "device", "up", "lab-usine/plt-0313/cnc.yml"])
         processes.append(device_proc)
         
         time.sleep(15)
 
-        print("\n [3/3] Bringing up equipments API...")
+        print("\n [3/4] Bringing up equipments API...")
         api_proc = subprocess.Popen(["ofa", "apps", "up", "openfactory-apps/api.yml"])
         processes.append(api_proc)
 
@@ -33,6 +39,22 @@ def main():
             if proc.poll() is None:
                 print(f"Terminating process PID {proc.pid}...")
                 proc.terminate()
+
+        print(" [1/3] Stopping OPCUA connector...")
+        connector_proc = subprocess.Popen(["ofa", "apps", "down", "/usr/local/share/openfactory-connectors/app_opcua_connector.yml"])
+        processes.append(connector_proc)
+        
+        time.sleep(10)
+
+        print("\n [2/3] Stopping OPC-UA device (lab-usine/plt-0313/cnc.yml)...")
+        device_proc = subprocess.Popen(["ofa", "device", "down", "lab-usine/plt-0313/cnc.yml"])
+        processes.append(device_proc)
+        
+        time.sleep(15)
+
+        print("\n [3/3] Stopping equipments API...")
+        api_proc = subprocess.Popen(["ofa", "apps", "down", "openfactory-apps/api.yml"])
+        processes.append(api_proc)
         
         time.sleep(2)
         print("Cleanup complete.")
